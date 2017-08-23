b4bc1165f2356fd100b82f940eeea06d06e3e868
AissaMilti <aissamilti@hotmail.com>
2017-08-23 15:57:54 +0200

    Menu view

--- OnlinePizza/Data/DbInitilizer.cs
+++ OnlinePizza/Data/DbInitilizer.cs
@@ -35,10 +35,18 @@ namespace OnlinePizza.Data
                 var cheese = new Ingredient { Name = "Cheese" };
                 var tomato = new Ingredient { Name = "Tomato" };
                 var ham = new Ingredient { Name = "Ham" };
+                var pasta = new Ingredient {Name = "Pasta"};
+                var chicken = new Ingredient {Name = "Chicken"};
+                var garlicSauce = new Ingredient {Name = "Garlic Sauce"};
 
+                //Pizza
                 var capricciosa = new Dish { Name = "Capricciosa", Price = 80 };
                 var margarita = new Dish { Name = "Margarita", Price = 70 };
                 var hawaii = new Dish { Name = "Hawaii", Price = 100 };
+                var kebabPizza = new Dish {Name = "Kebabpizza", Price = 80};
+
+                //Pasta
+                var pastaAlfredo = new Dish {Name = "Pasta Alfredo", Price = 90};
 
                 var margaritaCheese = new DishIngredient { Dish = margarita, Ingredient = cheese };
                 var margaritaHam = new DishIngredient { Dish = margarita, Ingredient = ham };
@@ -48,17 +56,24 @@ namespace OnlinePizza.Data
                 var capricciosaCheese = new DishIngredient { Dish = capricciosa, Ingredient = ham };
                 var capricciosaTomato = new DishIngredient { Dish = capricciosa, Ingredient = tomato };
 
+                var kebabTomato = new DishIngredient {Dish = kebabPizza, Ingredient = tomato};
+                var kebabGarlicSauce = new DishIngredient {Dish = kebabPizza, Ingredient = garlicSauce};
+
+                var alfredoChicken = new DishIngredient {Dish = pastaAlfredo, Ingredient = chicken};
+                var alfredoPasta = new DishIngredient {Dish = pastaAlfredo, Ingredient = pasta};
+
+
 
                 capricciosa.DishIngredients = new List<DishIngredient> { capricciosaCheese, capricciosaTomato };
-
-
                 margarita.DishIngredients = new List<DishIngredient> { margaritaCheese, margaritaHam };
                 hawaii.DishIngredients = new List<DishIngredient> { hawaiiCheese };
+                kebabPizza.DishIngredients = new List<DishIngredient>{kebabTomato, kebabGarlicSauce};
+                pastaAlfredo.DishIngredients = new List<DishIngredient>{alfredoPasta, alfredoChicken};
 
                 context.AddRange(tomato, ham, cheese);
 
-                context.DishIngredients.AddRange(margaritaCheese, hawaiiCheese, capricciosaCheese, capricciosaTomato, margaritaHam);
-                context.AddRange(capricciosa, margarita, hawaii);
+                context.DishIngredients.AddRange(margaritaCheese, hawaiiCheese, capricciosaCheese, capricciosaTomato, margaritaHam, kebabGarlicSauce, kebabTomato, alfredoPasta, alfredoChicken);
+                context.AddRange(capricciosa, margarita, hawaii, kebabPizza, pastaAlfredo);
                 context.SaveChanges();
             }
         }
